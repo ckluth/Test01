4ff5af5f82c4f845cc604d2a72b0e74d6c48541a
ckluth <christofer.kluth@outlook.com>
2015-08-20 13:13:07 +0200

    Habe viel gemacht.

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,9 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.Out.WriteLine("Hello World!");
+            
+            Console.WriteLine("Hello World!");
         }
     }
 }
